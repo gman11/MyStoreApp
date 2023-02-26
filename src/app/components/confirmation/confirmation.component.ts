c2535ec01077b02445cd470ce1e22e096e116428
Jesus Guerrero <guerrero.jec@gmail.com>
2023-02-25 23:07:44 -0700

    big changes

---
+++ src/app/components/confirmation/confirmation.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import {switchMap} from 'rxjs/operators';
+
+
+@Component({
+  selector: 'app-confirmation',
+  templateUrl: './confirmation.component.html',
+  styleUrls: ['./confirmation.component.css']
+})
+export class ConfirmationComponent implements OnInit {
+ fullName:string | null = "" ;
+ address:string | null = "" ;
+
+ creditCard:string | null = "" ;
+ total:string | null ="";
+
+  constructor(private route:ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.fullName =   this.route.snapshot.paramMap.get("fullName");
+    this.address =   this.route.snapshot.paramMap.get("address");
+    this.creditCard =   this.route.snapshot.paramMap.get("creditCard");
+    this.total =   this.route.snapshot.paramMap.get("total");
+
+  }
+
+}
